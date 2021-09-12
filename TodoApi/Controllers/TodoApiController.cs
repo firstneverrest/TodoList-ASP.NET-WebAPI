79503e33f651ed58b937a185142bcbd63ba6fb27
Chitsanupong <first30952@gmail.com>
2021-09-12 12:29:03 +0700

    feat: add post, put, delete

--- TodoApi/Controllers/TodoApiController.cs
+++ TodoApi/Controllers/TodoApiController.cs
@@ -9,7 +9,7 @@ using TodoApi.Models;
 namespace TodoApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/activities")]
     public class TodoApiController : ControllerBase
     {
         private readonly ILogger<TodoApiController> _logger;
@@ -20,8 +20,17 @@ namespace TodoApi.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get([FromHeader] string Authorization)
         {
+            // find token
+            try {
+                string[] authorization = Authorization.Split(' ');
+                string token = authorization[1];
+                if (token == null) return StatusCode(401, new {message = "Invalid Token"});
+            } catch {
+                return StatusCode(401, new {message = "Invalid Token"});
+            }
+
             var db = new AMCDbContext();
             var todoLists = db.Activities.Select(s => s);
             return Ok(todoLists);
@@ -29,8 +38,17 @@ namespace TodoApi.Controllers
 
         [HttpGet]
         [Route("{id}")]
-        public IActionResult Get(uint id)
+        public IActionResult Get(uint id, [FromHeader] string Authorization)
         {
+            // find token
+            try {
+                string[] authorization = Authorization.Split(' ');
+                string token = authorization[1];
+                if (token == null) return StatusCode(401, new {message = "Invalid Token"});
+            } catch {
+                return StatusCode(401, new {message = "Invalid Token"});
+            }
+
             var db = new AMCDbContext();
             var todoLists = db.Activities.Where(s => s.Id == id).Select(s => s);
 
@@ -38,5 +56,84 @@ namespace TodoApi.Controllers
             
             return Ok(todoLists);
         }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Activity todo, [FromHeader] string Authorization)
+        {
+            // find token
+            try {
+                string[] authorization = Authorization.Split(' ');
+                string token = authorization[1];
+                if (token == null) return StatusCode(401, new {message = "Invalid Token"});
+            } catch {
+                return StatusCode(401, new {message = "Invalid Token"});
+            }
+
+            try {
+                var db = new AMCDbContext();
+                db.Activities.Add(todo);
+                db.SaveChanges();
+            } catch (Exception e) {
+                return StatusCode(500, new {message = e.ToString()});
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Put([FromBody] Activity todo, [FromHeader] string Authorization, uint id)
+        {
+            // find token
+            try {
+                string[] authorization = Authorization.Split(' ');
+                string token = authorization[1];
+                if (token == null) return StatusCode(401, new {message = "Invalid Token"});
+            } catch {
+                return StatusCode(401, new {message = "Invalid Token"});
+            }
+
+            try {
+                var db = new AMCDbContext();
+                var todoLists = db.Activities.Where(s => s.Id == id).Select(s => s);
+                if (!todoLists.Any()) return NotFound();
+                var td = todoLists.First();
+                td.Id = id;
+                td.Name = todo.Name;
+                td.When = todo.When;
+                db.SaveChanges();
+            } catch (Exception e) {
+                return StatusCode(500, new {message = e.ToString()});
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromHeader] string Authorization, uint id)
+        {
+            // find token
+            try {
+                string[] authorization = Authorization.Split(' ');
+                string token = authorization[1];
+                if (token == null) return StatusCode(401, new {message = "Invalid Token"});
+            } catch {
+                return StatusCode(401, new {message = "Invalid Token"});
+            }
+
+            try {
+                var db = new AMCDbContext();
+                var todoList = db.Activities.Find(id);
+                db.Activities.Remove(todoList);
+                db.SaveChanges();
+            } catch (Exception e) {
+                return StatusCode(500, new {message = e.ToString()});
+            }
+
+            return Ok();
+        }
+
+
     }
 }
