01a453c23e4a6eae2b551bc8636d340dbd48accf
Chitsanupong <first30952@gmail.com>
2021-09-12 20:19:13 +0700

    feat: add hash function

---
+++ TodoApi/Utils/HashFunction.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text;
+
+namespace TodoApi.Utils
+{
+    public static class HashFunction
+    {
+        public static string[] CreateHashAndSalt(string password)
+        {
+            byte[] salt = new byte[128/8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256/8));
+
+            string[] results = {salt.ToString(), hashed};
+            return results;
+        }
+        public static bool CheckPassword(string password, string salt, string hash)
+        {
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: Encoding.ASCII.GetBytes(salt),
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256/8));
+
+            if (hash != hashed) return false;
+
+            return true;
+        }
+    }
+}
