b47f5e6aa6780f4d4af0c92779cce68fdc01eef7
Chitsanupong <first30952@gmail.com>
2021-10-04 20:40:10 +0700

    fix: change namespace

--- TodoApi/Controllers/Account.cs
+++ TodoApi/Controllers/Account.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace TodoApi.Utils
+namespace TodoApi.Controllers
 {
     public class Account
     {
